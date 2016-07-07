c48efc646cfda1f529a180a00b86df5e0db4c069
Junichi Ito <jit@sonicgarden.jp>
2016-07-08 08:02:41 +0900

    Update to Rails 4.2.6

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module ContactsRspec3Rails41
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_contacts_rspec_3_rails_4_1_session'
+Rails.application.config.session_store :cookie_store, key: '_contacts_rspec3_rails41_session'
