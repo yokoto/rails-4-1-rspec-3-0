a85038e4926cb87be3696af518b2576bdd020f55
Junichi Ito <jit@sonicgarden.jp>
2016-07-08 08:38:52 +0900

    Move generator settings to generators.rb

---
+++ config/initializers/generators.rb
@@ -0,0 +1,10 @@
+Rails.application.config.generators do |g|
+  g.test_framework :rspec,
+                   fixtures: true,
+                   view_specs: false,
+                   helper_specs: false,
+                   routing_specs: false,
+                   controller_specs: true,
+                   request_specs: false
+  g.fixture_replacement :factory_girl, dir: "spec/factories"
+end
